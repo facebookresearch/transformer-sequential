a5b603107b3784ad6d832cdcd5e4cc4aba65533d
Dexter Ju <da.ju.fr@gmail.com>
2022-09-28 08:38:43 -0700

    add staircase models

--- models/utils.py
+++ models/utils.py
@@ -6,6 +6,7 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
+from argparse import Namespace
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -36,3 +37,35 @@ def unskew(X):
 def pos_emb(args, sizes):
     X = torch.randn(*sizes)
     return nn.Parameter(X)
+
+def parse_layerwise(args, l):
+    args_copy = Namespace(**vars(args))
+    use_copy = False
+    if args.attn_lim_layerwise != "":
+        lims = args.attn_lim_layerwise.split(":")
+        args_copy.attn_lim = int(lims[l])
+        use_copy = True
+    if args.inner_hid_sz_layerwise != "":
+        sizes = args.inner_hid_sz_layerwise.split(":")
+        args_copy.inner_hid_sz = int(sizes[l])
+        use_copy = True
+    if args.attn_key_mode_layerwise != "":
+        mode = args.attn_key_mode_layerwise.split(":")
+        args_copy.attn_key_mode = mode[l]
+        use_copy = True
+    if args.nheads_layerwise != "":
+        nheads = args.nheads_layerwise.split(":")
+        args_copy.nheads = int(nheads[l])
+        use_copy = True
+    if args.head_dim_layerwise != "":
+        x = args.head_dim_layerwise.split(":")
+        args_copy.head_dim = int(x[l])
+        use_copy = True
+    if args.expire_span_layerwise != "":
+        assert args.expire_span
+        x = args.expire_span_layerwise.split(":")
+        args_copy.expire_span = x[l] == "1"
+        use_copy = True
+    if not use_copy:
+        args_copy = args
+    return args_copy
